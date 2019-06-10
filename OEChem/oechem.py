36598fe41ad1f780eeb42a9ed1cba4877a3e9a73
V <v@Vs-MacBook-Air.local>
2019-06-11 01:20:30 +0300

    readfrom

--- OEChem/oechem.py
+++ OEChem/oechem.py
@@ -4,17 +4,14 @@ from __future__ import print_function
 import sys, tarfile, gzip, os, time
 from io import BytesIO
 
-
-PATH = os.path.dirname(__file__)+"/../resultFiles"
-
-
-def downloadFiles(IDs):
-    global PATH
-    memoryFile = BytesIO()
-    with tarfile.open(fileobj=memoryFile, mode="w:gz") as tf:
+def downloadFiles(IDs, outfile, inputDir):
+    missedFiles = 0
+    i = 0
+    with tarfile.open(fileobj=outfile, mode="w:gz") as tf:
         for shortID in IDs:
+            i+=1
             ID = '{:09d}'.format(int(shortID)) # Leading zeros
-            gzFilePath = "{}/{}/{}/{}.gz".format(PATH, ID[0:3], ID[3:6], ID)
+            gzFilePath = "{}/{}/{}/{}.gz".format(inputDir, ID[0:3], ID[3:6], ID)
             if os.path.isfile(gzFilePath):
                 with gzip.open(gzFilePath, 'rb') as f:
                     data = f.read()
@@ -22,10 +19,11 @@ def downloadFiles(IDs):
                     tarinfo.size = len(data)
                     tf.addfile(tarinfo, BytesIO(data))
             else:
-                print("No such file: {}".format(gzFilePath), file=sys.stderr)
-    resultData = memoryFile.getvalue()
-    memoryFile.close()
-    return resultData
+                missedFiles += 1
+            if i % 100 == 0:
+                print("\rProgress: [{}/{}]. Missed files count: {}".format(i, len(IDs), missedFiles), file=sys.stderr, end='')
+                sys.stderr.flush()
+
 
 def check_ids(ids):
     result = []
@@ -39,8 +37,8 @@ def check_ids(ids):
 
 if __name__ == '__main__':
     import argparse
-    
-    parser = argparse.ArgumentParser(description="OEchem data downloader")
+    readFromPath = os.path.realpath(os.path.dirname(__file__)+"/../resultFiles/")
+    parser = argparse.ArgumentParser(description="OEchem data downloader", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-f", "--input-file", dest='inputFile', type=argparse.FileType('r'), metavar='FILE',
                         help="input file with space or newline separated IDs. For stdin use -")
     parser.add_argument("-i", "--ids", dest='ids', type=check_ids, metavar='LIST',
@@ -48,6 +46,9 @@ if __name__ == '__main__':
     parser.add_argument("-o", "--output-file", dest='outFile', type=argparse.FileType('w'), 
                         metavar='FILE', required=True,
                         help='Output archive name, like result.tar.gz. For stdout use -')     
+    parser.add_argument("-r", "--readfromdir", dest='readFromPath',  
+                        metavar='DIR', default = readFromPath,
+                        help='Directory to search input files.')     
 
     args = parser.parse_args()
 
@@ -58,10 +59,8 @@ if __name__ == '__main__':
     if args.ids:
         ids.extend(args.ids)
     if ids:
-        print("packing {} to {}".format(ids, args.outFile.name), file=sys.stderr)
-        resultZip = downloadFiles(ids)
-        args.outFile.write(resultZip)
-        args.outFile.close()
+        print("packing {} files to {}".format(len(ids), args.outFile.name), file=sys.stderr)
+        downloadFiles(IDs = ids, outfile = args.outFile, inputDir = readFromPath)
     else:
         parser.error ('Either --input-file or --ids is required.')
 
